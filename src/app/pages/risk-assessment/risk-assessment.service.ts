c1e390786bce86cd38ee53086b28a3330e1c75cb
Chedly <chedly.marzougui@gmail.com>
2022-09-11 14:27:07 +0100

    Refactor risk-assesment service

--- src/app/pages/risk-assessment/risk-assessment.service.ts
+++ src/app/pages/risk-assessment/risk-assessment.service.ts
@@ -2,24 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RiskAssessment } from './risk-assessment';
+import { environment } from "../../../environments/environment"
 
 @Injectable({
   providedIn: 'root'
 })
 export class RiskAssessmentService {
+  apiUrl = environment.baseApiUrl + "risk_assessment/"
 
   constructor(private http:HttpClient) { }
 
   postRiskAssessment(data:any){
-    return this.http.post<any>('http://localhost:3000/risk_assessment/',data);
+    return this.http.post<any>(this.apiUrl,data);
   }
   getRiskAssessment():Observable<RiskAssessment[]>{
-    return this.http.get<RiskAssessment[]>('http://localhost:3000/risk_assessment');
+    return this.http.get<RiskAssessment[]>(this.apiUrl);
   }
   putRiskAssessment(data:any, id:number){
-    return this.http.put<any>('http://localhost:3000/risk_assessment/'+id,data)
+    return this.http.put<any>(this.apiUrl+id,data)
   }
   deleteRiskAssessment(id:number){
-    return this.http.delete<any>('http://localhost:3000/risk_assessment/'+id)
+    return this.http.delete<any>(this.apiUrl+id)
   }
 }
